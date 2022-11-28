2e2910b8ad6037095ac02d781559ab43b269454c
saiprasanthg <saiprasanth.guthula@sjsu.edu>
2022-11-27 22:59:52 -0800

    added src

---
+++ src/Baggage.js
@@ -0,0 +1,27 @@
+import React from "react";
+function Baggage(){
+    return(
+        <div id="Baggage">
+  <table className="bagg-tab">
+  <tr>
+    <th>From</th>
+    <th>To</th>
+    <th>Flight</th>
+    <th>Departure</th>
+    <th>Arrival</th>
+    <th>Baggage</th>
+  </tr>
+  <tr>
+    <td>San Francisco International Airport <br />Terminal 2</td>
+    <td>London H cathrow  <br />Terminal 3</td>
+    <td>Ly317</td>
+    <td>17:20 <br/>4 dec </td>
+    <td>21:40 <br/>5 dec </td>
+    <td>Belt 2 </td>
+
+  </tr>
+</table>
+        </div>
+    )
+}
+export default Baggage;
