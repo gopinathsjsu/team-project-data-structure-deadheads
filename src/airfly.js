0a3962538c6b375ce8d9398b2a0737bc4cb3b68b
lokeshreddiee <111704189+lokeshreddiee@users.noreply.github.com>
2022-12-04 20:28:26 -0800

    update airfly.js

---
+++ src/airfly.js
@@ -0,0 +1,31 @@
+import React from "react";
+// import {BrowserRouter as Router, Link} from 'react-router-dom';
+
+function Employ(){
+    return(
+      
+        <div id="Passenger">
+           {/* <Router> */}
+      {/* <div> */}
+        {/* <Link to="/Passenger"> */}
+          {/* <button>Click</button> */}
+        {/* </Link> */}
+
+        {/* <br /> */}
+        {/* <br /> */}
+        {/* <a href="https://google.com" target="_blank" rel="noreferrer"> */}
+          {/* <button>Click</button> */}
+        {/* </a> */}
+      {/* </div> */}
+    {/* </Router> */}
+      <h1 className="login_heading">Welcome to AirFly</h1>
+      <button className="third">Flight Details</button>
+      <button className="third">Baggage Details</button>
+      <button className="third">Enable/Disable Arrival</button>
+      <button className="third">Enable/Disable Departure</button>
+      <button className="third">Assign Baggage Belt</button>
+    
+        </div>
+    )
+}
+export default Employ;
